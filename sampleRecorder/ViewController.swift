46e214c6f7f84af4041de2efa65d647a0883b936
ei17ringo <ei17ringo@gmail.com>
2016-05-27 08:51:50 +0800

    commit init

--- sampleRecorder/ViewController.swift
+++ sampleRecorder/ViewController.swift
@@ -7,12 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let fileManager = NSFileManager()
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    let fileName = "sample.caf"
 
+    @IBOutlet weak var btnRecord: UIButton!
+    
+    @IBOutlet weak var btnPlay: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupAudioRecorder()
+    }
+    
+    @IBAction func tapBtnRecord(sender: UIButton) {
+        audioRecorder?.record()
+    }
+    
+    @IBAction func tapBtnPlay(sender: UIButton) {
+        play()
+    }
+    
+    // 録音するために必要な設定を行う
+    // viewDidLoad時に行う
+    func setupAudioRecorder(){
+        // 再生と録音機能をアクティブにする
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try! session.setActive(true)
+        let recordSetting : [String : AnyObject] = [AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey : 16,
+            AVNumberOfChannelsKey : 2,
+            AVSampleRateKey : 44100.0
+        ]
+        
+        do{
+            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+        }catch{
+            print("初期設定でのエラー")
+        }
+    }
+    
+    // 再生
+    func play(){
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+        }catch{
+            print("再生時にエラー")
+        }
+        audioPlayer?.play()
+    
+    }
+    
+    // 録音するファイルのパスを取得（録音時、再生時に参照）
+    // swift2からstringByAppendingComponent
+    func documentFilePath()->NSURL {
+        let urls = fileManager.URLsForDirectory(.DocumentationDirectory, inDomains: .UserDomainMask) as [NSURL]
+
+        let dirURL = urls[0]
+        
+        return dirURL.URLByAppendingPathComponent(fileName)
+    
     }
 
     override func didReceiveMemoryWarning() {
